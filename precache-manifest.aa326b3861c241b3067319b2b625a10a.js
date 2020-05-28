8dd47b51131fa04a79ef5268b546b682342b7964
Rumesh Udash <rumesh.udas@gmail.com>
2020-05-28 15:57:56 +0545

    Updates

---
+++ precache-manifest.aa326b3861c241b3067319b2b625a10a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e43d92aa9feabe0158470691ed3ce5",
+    "url": "/react-flip-countdown/index.html"
+  },
+  {
+    "revision": "2130c3845e8ba0dbaf81",
+    "url": "/react-flip-countdown/static/css/main.b6ed6e08.chunk.css"
+  },
+  {
+    "revision": "8f5b1fa933017667fb42",
+    "url": "/react-flip-countdown/static/js/2.e3d73fee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flip-countdown/static/js/2.e3d73fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2130c3845e8ba0dbaf81",
+    "url": "/react-flip-countdown/static/js/main.d80f783c.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/react-flip-countdown/static/js/main.d80f783c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1dbbacc2fcc64c2bfc1",
+    "url": "/react-flip-countdown/static/js/runtime-main.250dd740.js"
+  }
+]);
